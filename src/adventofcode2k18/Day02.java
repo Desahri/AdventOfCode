870063812a097f2f1faa571f6690be4ac6692c75
desahri <didier96@live.nl>
2018-12-03 00:38:18 +0100

    Day 2, part 1: created basic structure

--- src/adventofcode2k18/Day02.java
+++ src/adventofcode2k18/Day02.java
@@ -8,5 +8,36 @@ public class Day02 {
     public static void main(String[] args) throws IOException {
         // TODO Auto-generated method stub
         ArrayList<String> in = GetInput.get("02.txt");
+        int two = 0;
+        int three = 0;
+
+        for(String id : in) {
+            if(hasExactlyTwo(id)) {
+                two++;
+            }
+            if (hasExactlyThree(id)) {
+                three++;
+            }
+        }
+
+        //calculate checksum
+        int checksum = two*three;
+        System.out.println(checksum);
+    }
+
+    /**
+     *
+     * returns whether the strings has a character that appears exactly twice
+     */
+    static boolean hasExactlyTwo(String id) {
+        return false;
+    }
+
+    /**
+     *
+     * returns whether the strings has a character that appears exactly three times
+     */
+    static boolean hasExactlyThree(String id) {
+        return false;
     }
 }
