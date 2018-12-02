14bccba98ef8d5d7e6c4e451ffd3082709638794
desahri <didier96@live.nl>
2018-12-02 19:49:26 +0100

    solution day 1 (1st part)

--- src/adventofcode2k18/Day01.java
+++ src/adventofcode2k18/Day01.java
@@ -6,7 +6,19 @@ import java.util.*;
 public class Day01 {
 	
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
 		ArrayList<String> in = GetInput.get("01.txt");
+		int ans = 0;
+		for(String line : in) {
+			String op = line.substring(0, 1);
+			String nr = line.substring(1);
+			assert (op.equals("+") || op.equals("-"));
+			if(op.equals("+")) {
+			    ans += Integer.parseInt(nr);
+			    continue;
+            }
+            ans -= Integer.parseInt(nr);
+
+		}
+		System.out.println("answer: " + ans);
 	}
 }
