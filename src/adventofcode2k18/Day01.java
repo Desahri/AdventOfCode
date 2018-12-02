5149546cf7366cc1c79f5db7415c3210fe00a8bd
desahri <didier96@live.nl>
2018-12-02 20:25:46 +0100

    solution day 1 (2nd part)

--- src/adventofcode2k18/Day01.java
+++ src/adventofcode2k18/Day01.java
@@ -7,6 +7,8 @@ public class Day01 {
 	
 	public static void main(String[] args) throws IOException {
 		ArrayList<String> in = GetInput.get("01.txt");
+		// Answer part 1
+		/*
 		int ans = 0;
 		for(String line : in) {
 			String op = line.substring(0, 1);
@@ -20,5 +22,27 @@ public class Day01 {
 
 		}
 		System.out.println("answer: " + ans);
+		*/
+		//answer part 2
+		int freq = 0;
+		List<Integer> frequencies = new ArrayList<>();
+		frequencies.add(freq);
+		while(true) {
+            for (String line : in) {
+                String op = line.substring(0, 1);
+                String nr = line.substring(1);
+                assert (op.equals("+") || op.equals("-"));
+                if (op.equals("+")) {
+                    freq += Integer.parseInt(nr);
+                } else {
+                    freq -= Integer.parseInt(nr);
+                }
+                if (frequencies.contains(freq)) {
+                    System.out.println("answer: " + freq);
+                    return;
+                }
+                frequencies.add(freq);
+            }
+        }
 	}
 }
