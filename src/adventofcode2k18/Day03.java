fe9786a2f2481912942ecc69e77b8106579fef81
desahri <didier96@live.nl>
2018-12-03 11:56:30 +0100

    Day 3, part 2: solution

--- src/adventofcode2k18/Day03.java
+++ src/adventofcode2k18/Day03.java
@@ -8,6 +8,8 @@ public class Day03 {
     public static void main(String[] args) throws IOException {
         // TODO Auto-generated method stub
         ArrayList<String> in = GetInput.get("03.txt");
+        //solution part 1
+        /*
         int[][] field = new int[1000][1000];
         for(String s :in) {
             String[] split = s.split(" ");
@@ -16,6 +18,57 @@ public class Day03 {
             fillField(field, corner, size);
         }
         System.out.println(countOverlaps(field));
+        */
+
+        //solution part 2
+        class FabricArea {
+            String id;
+            int x;
+            int y;
+            int length;
+            int height;
+
+            FabricArea(String id, String corner, String size) {
+                this.id = id;
+                String[] cornerA = corner.split(",");
+                String[] sizeA = size.split("x");
+                this.x = Integer.parseInt(cornerA[0]);
+                this.y = Integer.parseInt(cornerA[1].substring(0, cornerA[1].length()-1));
+
+                this.length = Integer.parseInt(sizeA[0]);
+                this.height = Integer.parseInt(sizeA[1]);
+            }
+
+            boolean overlaps(FabricArea area) {
+                if(area.x > this.x+this.length-1 || area.y > this.y+this.height-1) {
+                    return false;
+                }
+                if(area.x + area.length-1 < this.x || area.y + area.height-1 < this.y) {
+                    return false;
+                }
+                return true;
+            }
+        }
+        FabricArea[] areas = new FabricArea[in.size()];
+        int i = 0;
+        for(String s :in) {
+            String[] split = s.split(" ");
+            String id = split[0];
+            String corner = split[2];
+            String size = split[3];
+            areas[i] = new FabricArea(id, corner, size);
+            i++;
+        }
+        area1:
+        for(FabricArea ar : areas) {
+            for(FabricArea ar2 : areas) {
+                if(ar != ar2 && ar.overlaps(ar2)) {
+                    continue area1;
+                }
+            }
+            System.out.println(ar.id);
+            return;
+        }
     }
 
     static void fillField(int[][] field, String corner, String size) {
