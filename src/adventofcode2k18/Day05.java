bc50f4866de22a1b5f589492268189082c253841
desahri <didier96@live.nl>
2018-12-05 07:55:28 +0100

    Day 5, part 2: solution

--- src/adventofcode2k18/Day05.java
+++ src/adventofcode2k18/Day05.java
@@ -9,10 +9,30 @@ public class Day05 {
         // TODO Auto-generated method stub
         ArrayList<String> in = GetInput.get("05.txt");
 
+        //solution part 1
+        /*
         String chemical = in.get(0);
         System.out.println("answer: " + react(chemical).length());
+        */
 
+        //solution part 2
+        char[] ALL_SMALL = new char[26];
+        ALL_SMALL[0] = 'a';
+        for (int i = 1; i < 26; i++) {
+            ALL_SMALL[i] = (char) ('a' + i);
+        }
 
+        String chemical = in.get(0);
+        char[] chemArr = chemical.toCharArray();
+        int smallest = Integer.MAX_VALUE;
+        for (char letter : ALL_SMALL) {
+            char[] chemArr2 = removeAllChars(chemArr, letter);
+            int length = react(new String(chemArr2)).length();
+            if (length < smallest) {
+                smallest = length;
+            }
+        }
+        System.out.println("answer: " + smallest);
     }
 
     static String react(String chem) {
@@ -42,5 +62,30 @@ public class Day05 {
             r[i] = chem[i + passedIndex];
         }
         return r;
+    }
+
+    static char[] removeAllChars(char[] chem, char c) {
+        char small = Character.toLowerCase(c);
+        char large = Character.toUpperCase(c);
+        char[] r = new char[chem.length - countChars(chem, small) - countChars(chem, large)];
+        int passedIndex = 0;
+        for (int i = 0; i < chem.length; i++) {
+            if (chem[i] == small || chem[i] == large) {
+                passedIndex++;
+                continue;
+            }
+            r[i - passedIndex] = chem[i];
+        }
+        return r;
+    }
+
+    static int countChars(char[] chem, char c) {
+        int count = 0;
+        for (char x : chem) {
+            if (x == c) {
+                count++;
+            }
+        }
+        return count;
     }
 }
