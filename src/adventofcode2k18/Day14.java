0a77e2507e552c7dc30e7a4d7db6b0149bc430a0
desahri <didier96@live.nl>
2018-12-14 21:56:12 +0100

    Day 14, part 2: solution

--- src/adventofcode2k18/Day14.java
+++ src/adventofcode2k18/Day14.java
@@ -8,6 +8,9 @@ public class Day14 {
     public static void main(String[] args) throws IOException {
         // TODO Auto-generated method stub
         ArrayList<String> in = GetInput.get("14.txt");
+
+        //solution part 1
+        /*
         int recipeNr = Integer.parseInt(in.get(0));
 
         StringBuilder recipes = new StringBuilder("37");
@@ -21,5 +24,28 @@ public class Day14 {
             elf2 = (elf2 + (recipes.charAt(elf2) - '0') + 1) % recipes.length();
         }
         System.out.println("answer: " + recipes.substring(recipeNr, recipeNr + 10));
+        */
+
+        //solution part 2
+        String recipeNr = in.get(0);
+
+        StringBuilder recipes = new StringBuilder("37");
+        int elf1 = 0;
+        int elf2 = 1;
+
+        int i = 0;
+        while (/*!recipes.toString().contains(recipeNr)*/ i < 500000000) { //code that was commented out takes too long
+            int combi = (recipes.charAt(elf1) - '0') + (recipes.charAt(elf2) - '0');
+            recipes.append(combi);
+            elf1 = (elf1 + (recipes.charAt(elf1) - '0') + 1) % recipes.length();
+            elf2 = (elf2 + (recipes.charAt(elf2) - '0') + 1) % recipes.length();
+            i++;
+        }
+        for (int x = 0; x < recipes.length() - recipeNr.length(); x++) {
+            if (recipes.substring(x, x + recipeNr.length()).equals(recipeNr)) {
+                System.out.println("answer: " + (x));
+                break;
+            }
+        }
     }
 }
